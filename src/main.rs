e89351552ac6a2f8313fb056dd0bd1f5d40a4a74
Alexandria <28266167+alxpettit@users.noreply.github.com>
2022-08-09 23:56:27 -0700

    data type is gibberish but thats how I do things

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,23 @@ extern crate notify;
 use notify::{Watcher, RecursiveMode, watcher};
 
 struct App {
-
+    flags: Vec<String>,
+    child_args: Vec<String>,
+    child: Option<String>,
+    expecting_flags: bool
 }
 
 impl App {
 
     fn new() -> Self {
-        Self {  }
+        Self {
+            flags: Vec::<String>::new(),
+            child_args: Vec::<String>::new(),
+            child: None,
+            expecting_flags: true
+        }
     }
+
     fn main(&mut self) {
         // init logging
         if env::var("RUST_LOG").is_err() {
@@ -26,43 +35,35 @@ impl App {
         }
         pretty_env_logger::init();
 
+        
         // init local vars
-        let mut flags: Vec<String> = Vec::<String>::new();
-        let mut expecting_flags = true;
         let args: Vec<String> = env::args().skip(1).collect();
-        let mut child: Option<String> = None;
-        let mut child_args: Vec<String> = Vec::<String>::new();
 
         // parse args
         for arg in args {
-            if expecting_flags {
-                if arg.starts_with('-') {
-                    flags.push(arg);
-                } else {
-                    child = Some(arg);
-                    expecting_flags = false;
-                }
-            } else {
-                child_args.push(arg)
-            }
+            self.handle_arg(arg);
         }
 
-        for flag in &flags {
-            Self::handle_flag(&flag);
+        for flag in &self.flags {
+            self.handle_flag(&flag);
         }
 
-        if let Some(p) = child {
-            let res = shell(&p, &child_args);
-            if res.is_err() {
-
-            }
-            println!("{:#?}", res);
+        if let Some(p) = &self.child {
+            self.run_child_process(p);
         } else {
             error!("No child to execute. See usage instructions.");
         }
     }
 
-    fn handle_flag(flag: &String) {
+    fn run_child_process(&self, p: &String) {
+        let res = shell(&p, &self.child_args);
+        if res.is_err() {
+
+        }
+        println!("{:#?}", res);
+    }
+
+    fn handle_flag(&self, flag: &String) {
         if let Some(split_values) = flag.split_once("=") {
             let key = split_values.0.trim_start_matches('-');
             let value = split_values.1;
@@ -70,12 +71,31 @@ impl App {
             println!("key: {} value: {}", key, value);
     
         } else {
-            Self::handle_single_flag(flag);
+            self.handle_single_flag(flag);
         }
     }
 
-    fn handle_single_flag(flag: &str) {
+    fn handle_single_flag(&self, flag: &str) {
         todo!()
+    }
+
+    fn handle_arg(&mut self, arg: String) {
+        if self.expecting_flags {
+            if arg.starts_with('-') {
+                self.flags.push(arg);
+            } else {
+                self.child = Some(arg);
+                self.expecting_flags = false;
+            }
+        } else {
+            self.child_args.push(arg)
+        }
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
